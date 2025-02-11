5f997cc8f34eeb1b09c3b2a53d45cabc96a8cd0d
Cristopher Fuentes <crfuentes12@gmail.com>
2025-02-11 11:53:50 +0100

    update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,5 @@
 //landing-page/src/app/home/page.tsx
+import dynamic from 'next/dynamic'
 import Hero from "@/components/home/Hero";
 import OurClientsCarousel from '@/components/home/OurClientsCarousel';
 import WhyWeDoThis from '@/components/home/WhyWeDoThis';
@@ -6,10 +7,14 @@ import AboutUs from "@/components/home/AboutUs";
 import FAQ from "@/components/home/FAQ";
 import CTA from "@/components/home/CTA";
 import Roadmap from "@/components/home/Roadmap";
-import ScrollToTop from "@/components/ScrollToTop";
 import PricingWrapper from "@/components/home/PricingWrapper";
 import ComparisonMatrix from "@/components/home/ComparisonMatrix";
 import BannerCTA from "@/components/home/BannerCTA";
+
+// Dynamically import ScrollToTop with SSR disabled
+const ScrollToTop = dynamic(() => import("@/components/ScrollToTop"), {
+  ssr: false // This will only render the component on the client side
+});
 
 export default function Home() {
   return (

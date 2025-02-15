2f3a183bbf42ee34e3f94395a10d2d6a9b466256
Cristopher Fuentes <crfuentes12@gmail.com>
2025-02-15 09:23:47 +0100

    updates made

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,7 +1,6 @@
 //landing-page/src/components/home/Hero.tsx
 "use client";
 
-import { useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { ArrowRight, ArrowDown } from "lucide-react";
 import { useScroll } from "@/providers/scroll-provider";
@@ -21,7 +20,6 @@ const stats: Stat[] = [
 ];
 
 const Hero = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
   const { scrollToSection } = useScroll();
 
   return (

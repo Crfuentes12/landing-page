e26da4328719b9e3f4f8da10c0ecc4d8e7756c5f
Cristopher Fuentes <crfuentes12@gmail.com>
2025-03-11 13:31:31 +0100

    updates made

--- src/app/api/sitemap/route.ts
+++ src/app/api/sitemap/route.ts
@@ -3,13 +3,20 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const baseUrl = "https://sprintlaunchers.com";
-  
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url><loc>${baseUrl}/</loc></url>
-    <url><loc>${baseUrl}/home</loc></url>
-    <url><loc>${baseUrl}/about</loc></url>
-    <url><loc>${baseUrl}/pricing</loc></url>
+    <url>
+      <loc>${baseUrl}/</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <priority>1.00</priority>
+    </url>
+    <url>
+      <loc>${baseUrl}/home</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <priority>0.80</priority>
+    </url>
   </urlset>`;
 
   return new NextResponse(sitemap, {
